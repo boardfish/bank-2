7aceef0415d0d3852590e9c1b83e02a792abb6b7
Simon Fish <si@mon.fish>
2019-01-05 13:46:19 +0000

    Retrieve access token with callback

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,8 +4,15 @@ class RootController < ApplicationController
   end
 
   def callback
-    @code = params[:code]
     @state = params[:state]
+    @response = connection.post '/oauth2/token', URI.encode_www_form({
+      grant_type: 'authorization_code',
+      client_id: 'oauth2client_00009eUNchi7jOqB10g2yX',
+      client_secret: ENV.fetch('MONZO_CLIENT_SECRET'),
+      redirect_uri: 'http://localhost:3000/callback',
+      code: params[:code]
+    })
+    session[:access_token] = JSON.parse(@response.body)['access_token']
   end
 
   def logout
@@ -17,5 +24,14 @@ class RootController < ApplicationController
     "&redirect_uri=#{CGI.escape 'http://localhost:3000/callback'}" \
     '&response_type=code' \
     '&state=foobar'
+  end
+
+  def connection(raise_error: false, token: nil)
+    Faraday.new(url: 'https://api.monzo.com/') do |faraday|
+      faraday.use Faraday::Response::RaiseError if raise_error
+      faraday.adapter Faraday.default_adapter
+      faraday.headers['Authorization'] = "Bearer #{token}" if token
+      faraday.request  :url_encoded             # form-encode POST params
+    end
   end
 end
