4452b3f52b02aa78016eb8898b9fbb6fb050bd78
Simon Fish <si@mon.fish>
2019-01-05 13:17:35 +0000

    Hardcode login URL
    To be replaced with i18n once everything clicks

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,10 +1,19 @@
 class RootController < ApplicationController
   def index
+    @monzo_login_url = monzo_login_url
   end
 
   def callback
   end
 
   def logout
+  end
+
+  def monzo_login_url
+    'https://auth.monzo.com/' \
+    "?client_id=oauth2client_00009eUNchi7jOqB10g2yX" \
+    "&redirect_uri=#{CGI.escape 'http://localhost:3000/callback'}" \
+    '&response_type=code' \
+    '&state=foobar'
   end
 end
