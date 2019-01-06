d625a229b458d50fb21d9d9e9c2649cdb0905e94
Simon Fish <si@mon.fish>
2019-01-06 13:55:47 +0000

    Display currencies as formatted monetary values

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'monzo'
 # Faraday
 gem 'faraday'
 
+# RubyMoney
+gem 'money'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -7,4 +7,12 @@ module RootHelper
     category = transaction.metadata[:oauth2client_00009eUNchi7jOqB10g2yX_category] || transaction.category
     category.humanize
   end
+
+  def to_currency(object)
+    if object.is_a?(Monzo::Transaction)
+      Money.new(object.amount, object.currency).format
+    elsif object.is_a?(Monzo::Pot) or object.is_a?(Monzo::Balance)
+      Money.new(object.balance, object.currency).format
+    end
+  end
 end
