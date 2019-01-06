6a36b89c7ce6d8a9317b9f1f21d125d292377356
Simon Fish <si@mon.fish>
2019-01-06 13:30:15 +0000

    Display categories more cleanly

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -2,4 +2,9 @@ module RootHelper
   def authenticated?
     session[:access_token]
   end
+
+  def category(transaction)
+    category = transaction.metadata[:oauth2client_00009eUNchi7jOqB10g2yX_category] || transaction.category
+    category.humanize
+  end
 end
