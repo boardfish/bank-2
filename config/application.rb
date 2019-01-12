f8e03e864506a5259211501f77d5d879cbe489b8
Simon Fish <si@mon.fish>
2019-01-12 14:36:08 +0000

    Define locale for Money to skip deprecation error

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Bank
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Money.locale_backend = :i18n
   end
 end
